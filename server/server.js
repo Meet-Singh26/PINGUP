d986e0bf4ada8db5b6303cbed6642dd23178e3e0
Meet-Singh26 <meet-singh2004@hotmail.com>
2025-08-14 20:17:50 +0530

    backend complete

--- server/server.js
+++ server/server.js
@@ -13,6 +13,9 @@ import { inngest, functions } from "./inngest/index.js";
 import { serve } from "inngest/express";
 import { clerkMiddleware } from "@clerk/express";
 import userRouter from "./routes/userRoutes.js";
+import postRouter from "./routes/postRoutes.js";
+import storyRouter from "./routes/storyRoutes.js";
+import messageRouter from "./routes/messageRoutes.js";
 
 const PORT = process.env.PORT;
 const app = express();
@@ -23,10 +26,12 @@ app.use(express.json());
 app.use(cors());
 app.use(clerkMiddleware());
 
-app.use("/api/user", userRouter);
-
 app.get("/", (req, res) => res.send("Server is running"));
 app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use("/api/user", userRouter);
+app.use("/api/post", postRouter);
+app.use("/api/story", storyRouter);
+app.use("/api/message", messageRouter);
 
 app.listen(PORT, () => {
   console.log("Server is running on port", PORT);
