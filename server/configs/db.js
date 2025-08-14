87431a3847571171b3d80030cbdd19dd655b7893
Meet-Singh26 <meet-singh2004@hotmail.com>
2025-08-14 09:57:05 +0530

    fixed url

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,3 +1,11 @@
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
+
 import mongoose from "mongoose";
 
 const connectDB = async () => {
@@ -14,7 +22,7 @@ const connectDB = async () => {
       console.log("Database Disconnected");
     });
 
-    await mongoose.connect(`${process.env.MONGODB_URL}/pingup`);
+    await mongoose.connect(process.env.MONGODB_URL);
   } catch (error) {
     console.error("Failed to connect to database:", error.message);
     process.exit(1); // Exit process with failure
